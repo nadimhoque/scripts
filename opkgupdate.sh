ee73954643d907d8230ccd7320041786c62829ca
Nadim Hoque <nadimhoque@gmail.com>
2022-04-08 03:23:18 -0700

    The script will not print the upgradable packages

--- opkgupdate.sh
+++ opkgupdate.sh
@@ -2,6 +2,10 @@
 
 #This is to upgrade the packages on openwrt
 opkg update
+echo "upgradable packages"
+opkg list-upgradable
+echo ""
+echo "performing the upgrade"
 for package in $(opkg list-upgradable | awk '{print $1}'); do 
 	opkg upgrade $package 
 done
